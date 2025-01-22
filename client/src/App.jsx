e8c8a06ef934d7018428ce5b677ae87eb5ee238f
aishastri <132866061+Srinu2023mca@users.noreply.github.com>
2025-01-22 13:29:41 +0530

    added login screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
-function App() {
-    return <h1>hi</h1>;
-}
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./components/Dashboard";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoute from "./router/auth.route.js"
+import authRoute from "./router/auth.route.js";
 import cookieParser from "cookie-parser";
 import connectDB from "./db/index.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -12,25 +13,27 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+app.use(cors());
+
 connectDB()
-    .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.log(`Unable to connect mongoDB server.... ${err}`);
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
     });
+  })
+  .catch((err) => {
+    console.log(`Unable to connect mongoDB server.... ${err}`);
+  });
 
 app.use("/api/v1/auth", authRoute);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
 
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });

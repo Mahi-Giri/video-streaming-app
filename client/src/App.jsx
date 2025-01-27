94e07064723f51ce92c07a10a083b051ab9d63f8
Srinu Vemula <vemulasrinuiso@gmail.com>
2025-01-27 15:12:04 +0530

    update navbar adminRoute and add tuseTranslation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,25 +11,36 @@ import Profile from "./pages/Profile";
 import Navbar from "./pages/Navbar";
 import "./App.css";
 import Subscription from "./pages/Subscription";
-
+import AdminRoute from "./components/AdminRoute";
+import { useSelector } from "react-redux";
+import Settings from "./pages/Settings";
 const App = () => {
     const [isNavbarExpanded, setNavbarExpanded] = useState(false);
+    const { currentUser } = useSelector((store) => store.user);
     return (
         <Router>
-            <Navbar setNavbarExpanded={setNavbarExpanded} />
+            {currentUser && <Navbar setNavbarExpanded={setNavbarExpanded} />}
             <div
-                className={`home-container ${isNavbarExpanded ? "expanded" : ""
-                    }`}
+                className={`home-container ${
+                    isNavbarExpanded ? "expanded" : ""
+                }`}
             >
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
                     <Route element={<PrivateRoute />}>
                         <Route path="/" element={<Home />} />
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        <Route path="/Subscription" element={<Subscription />} />
+                        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+                        <Route
+                            path="/Subscription"
+                            element={<Subscription />}
+                        />
+                        <Route path='/settings' element={<Settings/>}/>
                         <Route path="/profile" element={<Profile />} />
                         <Route path="/upload" element={<Upload />} />
+                        <Route element={<AdminRoute />}>
+                            <Route path="/dashboard" element={<Dashboard />} />
+                        </Route>
                     </Route>
                 </Routes>
             </div>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
+import languageSlice from "./languageSlice"
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
     user: userSlice,
+    language:languageSlice
 });
 
 const persistConfig = {
